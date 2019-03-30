1b31df6a0d8bd35fe69d185f9c3b9e93b1ef7b89
Eesa Patel <ep.94@hotmail.com>
2019-03-30 17:16:27 +0000

    install react-burgers; toggle burger function

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,38 @@
-import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
+import { Squeeze } from 'react-burgers';
 
-const Navbar = () => {
-  return (
-    <nav className="d-flex justify-content-between align-items-center">
-        <Link to='/' className="brandLink">
-            <h1 className="brandName montserrat-6">Eesa Patel.</h1>
-        </Link>
+class Navbar extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            active: false
+        }
+
+    }
     
-    </nav>
-  )
+    toggleBurger = () => {
+        this.setState({
+            active: !this.state.active
+        });
+    }
+
+
+    render() {
+
+        return (
+            <nav className="d-flex justify-content-between align-items-center">
+                <Link to='/' className="brandLink">
+                    <h1 className="brandName montserrat-6">Eesa Patel.</h1>
+                </Link>
+                <Squeeze
+                    active={this.state.active} 
+                    onClick={() => { this.toggleBurger(); }} />
+            </nav>
+        )
+    }
 }
 
 export default Navbar
