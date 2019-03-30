a3555a21c85955cdb648d669453ef6b8d94e42af
Eesa Patel <ep.94@hotmail.com>
2019-03-30 16:13:48 +0000

    convert brand name to Link; change existing colours to use primary and secondary variables

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,13 @@
-import React from 'react'
-import './Navbar.css'
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
   return (
     <nav className="d-flex justify-content-between align-items-center">
-        <h1 className="brand-name montserrat-6">Eesa Patel.</h1>
+        <Link to='/' className="brandLink">
+            <h1 className="brandName montserrat-6">Eesa Patel.</h1>
+        </Link>
     
     </nav>
   )
