2ae70fae37f6221ae7ef6ff2f0eeed95c914d543
Eesa Patel <ep.94@hotmail.com>
2019-04-06 20:44:03 +0100

    create about page layout

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Navbar from './Navbar';
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col, Card, CardHeader, CardBody, CardText } from 'reactstrap';
+import './About.css'
 
 
 export class About extends Component {
@@ -18,7 +19,59 @@ export class About extends Component {
             <div>
                 <Navbar />
                 <Container fluid className="pageContainer aboutContainer text-center">
-                    <h1>about me</h1>
+                    <h1 className="montserrat-9 mb3">About Me</h1>
+                    <Row>
+                        <Col
+                        xs={{ size: 10, offset: 1 }}
+                        md={{ size: 6, offset: 0}}
+                        xl={{ size: 4, offset: 2 }}
+                        className="mb2"
+                        >
+                            <Card>
+                                <CardHeader>
+                                    <h2 className="montserrat-7">Background</h2>
+                                </CardHeader>
+                                <CardBody>
+                                    <CardText>
+                                        <p className="montserrat-5 mb1">I am a front end dev</p>
+                                    </CardText>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                        <Col
+                        xs={{ size: 10, offset: 1 }}
+                        md={{ size: 6, offset: 0}}
+                        xl={{ size: 4, offset: 0 }}
+                        className="mb2"
+                        >
+                            <Card>
+                                <CardHeader>
+                                    <h2 className="montserrat-7">Skills</h2>
+                                </CardHeader>
+                                <CardBody>
+                                    <CardText>
+                                        <p className="montserrat-5 mb1">These are my skillz</p>
+                                    </CardText>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                        <Col
+                        xs={{ size: 10, offset: 1 }}
+                        xl={{ size: 8, offset: 2 }}
+                        className="mb2"
+                        >
+                            <Card>
+                                <CardHeader>
+                                    <h2 className="montserrat-7">FAQ</h2>
+                                </CardHeader>
+                                <CardBody>
+                                    <CardText>
+                                        <p className="montserrat-5 mb1">random shite</p>
+                                    </CardText>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    </Row>
                 </Container>
             </div>
         )
