f057784ea046acef91a4d1f6d997bd5eef2d9433
Eesa Patel <ep.94@hotmail.com>
2019-04-27 19:58:06 +0100

    crossover project image and information; add hover fx and style

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'reactstrap';
+import { Row, Col, Button } from 'reactstrap';
 import './Project.css';
 
 
@@ -8,8 +8,27 @@ export class Project extends Component {
         super(props)
 
         this.state = {
-            
+            active: false
         }
+
+        this.mouseEnter = this.mouseEnter.bind(this);
+    }
+
+    
+    mouseEnter = () => {
+        this.setState({
+            active: true
+        }, () => {
+            document.addEventListener('mouseleave', this.mouseLeave);
+        })
+    }
+
+    mouseLeave = () => {
+        this.setState({
+            active: false
+        }, () => {
+            document.removeEventListener('mouseleave', this.mouseLeave)
+        })
     }
     
 
@@ -19,16 +38,30 @@ export class Project extends Component {
             backgroundImage: this.props.image
         }
 
+        
+
         return (
-            <Row className="projectRow">
-                <Col xs="12" lg="6">
-                    <div className="projectImage h-100 w-100 d-flex justify-content-center align-items-center" style={projectImage}>
+            <Row noGutters className="projectRow">
+                <Col xs="12" lg={{ size: 6, offset: 1 }}>
+                    <div 
+                        className="projectImage h-100 w-100 d-flex justify-content-center align-items-center" 
+                        style={projectImage}
+                        onMouseEnter={this.mouseEnter}
+                        onMouseLeave={this.mouseLeave}
+                    >
+                        <div className={this.state.active ? "projectOverlay projectOverlayActive" : "projectOverlay"}>
+                            <Button href={this.props.url} target="_blank" className={this.state.active ? "projectLink projectLinkActive" : "projectLink"}>
+                            View Site
+                            </Button>
+                        </div>
                     </div>
                 </Col>        
-                <Col xs="12" lg="6">
-                    <div className="projectInformation h-100 w-100 d-flex justify-content-center align-items-center flex-column">
-                        <h1 className="projectTitle montserrat-7 mb2">{this.props.name}</h1>
-                        <p className="montserrat-5 mb2">{this.props.description}</p>
+                <Col xs="12" lg="5" className={this.state.active ? "projectInformationCol projectInformationColActive" : "projectInformationCol"}>
+                    <div className="h-100 w-100 d-flex justify-content-center align-items-start flex-column">
+                        <div className="projectInformation">
+                            <h1 className="projectTitle montserrat-7 mb2 text-left">{this.props.name}</h1>
+                            <p className="projectDescription montserrat-5 text-left">{this.props.description}</p>
+                        </div>
                     </div>
                 </Col>   
             </Row>     
