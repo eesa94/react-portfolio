bf06a3fe4fe888760ae40c371138241a348bba9f
Eesa Patel <ep.94@hotmail.com>
2019-04-27 16:58:15 +0100

    create portfolio page component

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar';
 import { Container, Row, Col } from 'reactstrap';
+import './Portfolio.css'
 
 
 export class Portfolio extends Component {
@@ -8,7 +9,18 @@ export class Portfolio extends Component {
         super(props)
 
         this.state = {
-            
+            projects: [
+                {
+                    name: "Project 1",
+                    url: "",
+                    github: "",                    
+                },
+                {
+                    name: "Project 2",
+                    url: "",
+                    github: "",                    
+                },
+            ]
         }
     }
 
@@ -18,7 +30,20 @@ export class Portfolio extends Component {
             <div>
                 <Navbar />
                 <Container fluid className="pageContainer portfolioContainer text-center">
-                    <h1>Portfolio</h1>
+                    <h1 className="montserrat-9 mb3">Portfolio</h1>
+                    <Row noGutters>
+                        {
+                            this.state.projects.map(project => 
+                                <Col xs="12" lg="6">
+                                    <div className="projectSquare">
+                                        <div className="projectOverlay h-100 w-100 d-flex justify-content-center align-items-center">
+                                            <h1 className="projectTitle">{project.name}</h1>
+                                        </div>
+                                    </div>
+                                </Col>
+                            )
+                        }
+                    </Row>
                 </Container>
             </div>
         )
