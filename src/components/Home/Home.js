176daeb485c35472d2f07fe2221de8c222daa037
eesa94 <30774374+eesa94@users.noreply.github.com>
2019-08-03 16:31:52 +0100

    bring navbar back into play, add to portfolio page and change primary & secondary root colours

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,9 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import './Home.css';
 import { Container, Row, Col, Button } from 'reactstrap';
 import { Link, withRouter } from 'react-router-dom';
 import home_background from '../../img/home_background.jpg';
 import SocialLinks from '../SocialLinks';
-
-import AboutMe from './AboutMe';
-import PortfolioPreview from './PortfolioPreview';
 
 const Home = () => {
   const bgImage = {
@@ -14,61 +11,53 @@ const Home = () => {
   };
 
   return (
-    <Fragment>
-      <Container fluid className='heroContainer text-center' style={bgImage}>
-        <div className='homeInner'>
-          <h1 className='welcomeHeader montserrat-9 mb3'>Welcome</h1>
-          <h3 className='welcomeSubHeader montserrat-5 mb3'>
-            Find out more about me, or view my portfolio, by clicking below.
-          </h3>
+    <Container fluid className='heroContainer text-center' style={bgImage}>
+      <div className='homeInner'>
+        <h1 className='welcomeHeader montserrat-9 mb3'>Welcome</h1>
+        <h3 className='welcomeSubHeader montserrat-5 mb3'>
+          Find out more about me, or view my portfolio, by clicking below.
+        </h3>
 
-          <Row className='mb3'>
-            <Col xs='6' className='text-right'>
-              <Link to='/about'>
-                <Button className='homeButton montserrat-5 shadow'>
-                  About Me
-                </Button>
-              </Link>
-            </Col>
-            <Col xs='6' className='text-left'>
-              <Link to='/portfolio'>
-                <Button className='homeButton montserrat-5 shadow'>
-                  View Work
-                </Button>
-              </Link>
-            </Col>
-          </Row>
+        <Row className='mb3'>
+          <Col xs='6' className='text-right'>
+            <Link to='/about'>
+              <Button className='homeButton montserrat-5 shadow'>
+                About Me
+              </Button>
+            </Link>
+          </Col>
+          <Col xs='6' className='text-left'>
+            <Link to='/portfolio'>
+              <Button className='homeButton montserrat-5 shadow'>
+                View Work
+              </Button>
+            </Link>
+          </Col>
+        </Row>
 
-          <Row className='mb2'>
-            <Col xs={{ size: 8, offset: 2 }}>
-              <img
-                src={require('../../img/headshot.jpg')}
-                alt='headshot'
-                className='img-fluid homeHeadshot'
-              />
-            </Col>
-          </Row>
+        <Row className='mb2'>
+          <Col xs={{ size: 8, offset: 2 }}>
+            <img
+              src={require('../../img/headshot.jpg')}
+              alt='headshot'
+              className='img-fluid homeHeadshot'
+            />
+          </Col>
+        </Row>
 
-          <Row className='mb1'>
-            <Col xs='12' className='text-center'>
-              <h2 className='homeName montserrat-9 mb1'>Eesa Patel.</h2>
-              <h3 className='welcomeSubHeader montserrat-5 mb1'>
-                Front End Developer
-              </h3>
-              <h4 className='welcomeSubHeader montserrat-5'>Leeds, UK</h4>
-            </Col>
-          </Row>
+        <Row className='mb1'>
+          <Col xs='12' className='text-center'>
+            <h2 className='homeName montserrat-9 mb1'>Eesa Patel.</h2>
+            <h3 className='welcomeSubHeader montserrat-5 mb1'>
+              Front End Developer
+            </h3>
+            <h4 className='welcomeSubHeader montserrat-5'>Leeds, UK</h4>
+          </Col>
+        </Row>
 
-          <SocialLinks />
-        </div>
-      </Container>
-      <Container fluid className='homeContainer'>
-        <AboutMe />
-      </Container>
-      <Container fluid className='homeContainer'>
-        <PortfolioPreview />
-      </Container>
-    </Fragment>
+        <SocialLinks />
+      </div>
+    </Container>
   );
 };
 
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Container } from 'reactstrap';
+import Navbar from '../Navigation/Navbar';
 import Project from './Project';
 import { projects } from '../../store/projectsData';
 
 const Portfolio = () => {
   return (
-    <Container fluid style={{ padding: '0' }}>
-      {projects.map((project, index) => (
-        <Project
-          key={index}
-          index={index}
-          name={project.name}
-          image={project.image}
-          description={project.description}
-          url={project.url}
-          github={project.github}
-          tech={project.tech}
-        />
-      ))}
-    </Container>
+    <Fragment>
+      <Navbar />
+      <Container fluid style={{ padding: '80px 0 0' }}>
+        {projects.map((project, index) => (
+          <Project
+            key={index}
+            index={index}
+            name={project.name}
+            image={project.image}
+            description={project.description}
+            url={project.url}
+            github={project.github}
+            tech={project.tech}
+          />
+        ))}
+      </Container>
+    </Fragment>
   );
 };
 
