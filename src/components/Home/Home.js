27cd2e3b07bd47a2583bfbdeb774251a576aefd0
eesa94 <30774374+eesa94@users.noreply.github.com>
2019-08-10 18:37:38 +0100

    try a different home layout

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,52 +12,76 @@ const Home = () => {
   };
 
   return (
-    <Container fluid className='heroContainer text-center' style={bgImage}>
-      <div className='homeInner white'>
-        <h1 className='welcomeHeader montserrat-9 mb3'>Welcome</h1>
-        <h3 className='welcomeSubHeader montserrat-5 mb3'>
-          Find out more about me, or view my portfolio, by clicking below.
-        </h3>
+    <Container fluid className='heroContainer text-left' style={bgImage}>
+      <Row>
+        <Col lg={{ size: 11, offset: 1 }}>
+          <h1 className='welcomeHeader montserrat-9'>Eesa Patel.</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col lg={{ size: 10, offset: 2 }} style={{ marginBottom: '100px' }}>
+          <h3 className='montserrat-7 white'>Welcome to my personal site.</h3>
+        </Col>
 
-        <Row className='mb3'>
-          <Col xs='6' className='text-right'>
-            <Link to='/about'>
-              <Button className='homeButton montserrat-5 shadow'>
-                About Me
-              </Button>
-            </Link>
-          </Col>
-          <Col xs='6' className='text-left'>
+        <Col lg={{ size: 9, offset: 3 }}>
+          <h3 className='welcomeSubHeader montserrat-7 white' id='line1'>
+            front end developer.
+          </h3>
+
+          <h3 className='welcomeSubHeader montserrat-7 white' id='line2'>
+            Leeds.
+          </h3>
+
+          <h3 className='welcomeSubHeader montserrat-7 white' id='line3'>
+            modern, aesthetic minimalism.
+          </h3>
+
+          {/* <Row className='mb3'>
+              <Col xs='6' className='text-right'>
+                <Link to='/about'>
+                  <Button className='homeButton montserrat-5 shadow'>
+                    About Me
+                  </Button>
+                </Link>
+              </Col>
+              <Col xs='6' className='text-left'>
+                <Link to='/portfolio'>
+                  <Button className='homeButton montserrat-5 shadow'>
+                    View Work
+                  </Button>
+                </Link>
+              </Col>
+            </Row>
+ */}
+          {/* <Row className='mb2'>
+              <Col xs={{ size: 8, offset: 2 }}>
+                <img
+                  src={require('../../img/headshot.jpg')}
+                  alt='headshot'
+                  className='img-fluid homeHeadshot'
+                />
+              </Col>
+            </Row> */}
+
+          {/* <SocialLinks /> */}
+        </Col>
+      </Row>
+      <Row>
+        <Col lg={{ size: 2, offset: 8 }}>
+          <div className='linksContainer'>
             <Link to='/portfolio'>
               <Button className='homeButton montserrat-5 shadow'>
-                View Work
+                View Portfolio
               </Button>
             </Link>
-          </Col>
-        </Row>
-
-        <Row className='mb2'>
-          <Col xs={{ size: 8, offset: 2 }}>
-            <img
-              src={require('../../img/headshot.jpg')}
-              alt='headshot'
-              className='img-fluid homeHeadshot'
-            />
-          </Col>
-        </Row>
-
-        <Row className='mb1'>
-          <Col xs='12' className='text-center'>
-            <h2 className='homeName montserrat-9 mb1'>Eesa Patel.</h2>
-            <h3 className='welcomeSubHeader montserrat-5 mb1'>
-              Front End Developer
-            </h3>
-            <h4 className='welcomeSubHeader montserrat-5'>Leeds, UK</h4>
-          </Col>
-        </Row>
-
-        <SocialLinks />
-      </div>
+            <Link to='/contact'>
+              <Button className='homeButton montserrat-5 shadow'>
+                Contact Me
+              </Button>
+            </Link>
+          </div>
+        </Col>
+      </Row>
     </Container>
   );
 };
